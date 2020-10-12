651269336084bd21c48fbb7afbcd7ffd36b32a98
KboRodo <61941560+KboRodo@users.noreply.github.com>
2020-10-12 18:49:42 +0200

    Działający lightbox

--- cw1-Lightbox/script.js
+++ cw1-Lightbox/script.js
@@ -1,6 +1,20 @@
-const firstimg = document.querySelector('#test')
-firstimg.addEventListener('click', showLightbox)
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
 
-function showLightbox () {
-  console.log('showlight')
-}
+const images = document.querySelectorAll('img')
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active')
+    const img = document.createElement('img')
+    img.src = image.src
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(img)
+  })
+})
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+  lightbox.classList.remove('active')
+})
