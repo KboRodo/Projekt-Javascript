e1046595156bed95df6ca3cce514fcb76334419e
KboRodo <61941560+KboRodo@users.noreply.github.com>
2020-10-13 13:40:50 +0200

    dodano przewijanie zdjęć

--- cw1-Lightbox/script.js
+++ cw1-Lightbox/script.js
@@ -1,20 +1,37 @@
-const lightbox = document.createElement('div')
-lightbox.id = 'lightbox'
-document.body.appendChild(lightbox)
+const images = document.querySelectorAll('.grid img')
+const lightbox = document.querySelector('.lightbox')
+const prev = document.querySelector('#prev')// przycisk poprzednie zdjecie
+const next = document.querySelector('#next')// przycisk nastepne zdjecie
+const close = document.querySelector('#close')// przycisk zamykanie lightboxa
+var prevEl// nastepny element
+var nextEl// poprzedni element
 
-const images = document.querySelectorAll('img')
-images.forEach(image => {
-  image.addEventListener('click', e => {
-    lightbox.classList.add('active')
-    const img = document.createElement('img')
-    img.src = image.src
-    while (lightbox.firstChild) {
-      lightbox.removeChild(lightbox.firstChild)
-    }
-    lightbox.appendChild(img)
-  })
+close.addEventListener('click', hideLightbox)// zamykanie lighboxa
+function hideLightbox () {
+  lightbox.classList.remove('visible')
+}
+
+for (let index = 0; index < images.length; index++) {
+  const img = images[index]
+  img.addEventListener('click', showLightbox)
+}
+
+function showLightbox (ev) { // otwieranie lightboxa
+  const lightboxImg = document.querySelector('.lightbox img')
+  lightboxImg.src = ev.target.src
+  lightbox.classList.add('visible')
+  prevEl = ev.target.previousElementSibling
+  nextEl = ev.target.nextElementSibling
+}
+next.addEventListener('click', function () { // przwijanie zdjec do przodu
+  console.log('next', nextEl)
+  hideLightbox()
+  nextEl.click()
 })
-lightbox.addEventListener('click', e => {
-  if (e.target !== e.currentTarget) return
-  lightbox.classList.remove('active')
+
+prev.addEventListener('click', function () {
+  console.log('prev-', prevEl)
+  console.log('next', nextEl)
+  hideLightbox()
+  prev.click()
 })
