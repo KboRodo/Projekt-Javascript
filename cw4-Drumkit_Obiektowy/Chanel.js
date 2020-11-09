2c87100085289cbc2c997319824bcb8b100a5466
KboRodo <61941560+KboRodo@users.noreply.github.com>
2020-11-09 20:43:56 +0100

    dodano nagrywanie na wiele kanałów

--- cw4-Drumkit_Obiektowy/Chanel.js
+++ cw4-Drumkit_Obiektowy/Chanel.js
@@ -1,25 +1,30 @@
 
 function Chanel () {
-  this.Tracks = []
+  this.Tracks = [] // tablica w której przechowywane są dzwięki
 
-  this.addSound = (sound) => {
+  this.addSound = (sound) => { // dodanie dzwieku do tablicy
     this.Tracks.push(sound)
   }
 
-  this.StartRecording = () => {
+  this.startRecording = () => { // rozpoczecie nagrywania
+    this.Tracks.length = 0
     this.recordStartDate = Date.now()
   }
 
-  this.playSounds = () =>
+  this.playSounds = () => { // odtwarzanie nagranych dzwieków
     this.Tracks.forEach(sound => {
       setTimeout(
         () => {
           sound.playSound()
         },
-        1000
+        sound.soundDate - this.recordStartDate
       )
     }
     )
+  }
+  this.playNewest = () => { // odtworzenie ostatniego dodanego dziwęku
+    this.Tracks[this.Tracks.length - 1].playSound()
+  }
 }
 
 export default Chanel
--- cw4-Drumkit_Obiektowy/script.js
+++ cw4-Drumkit_Obiektowy/script.js
@@ -1,2 +1,80 @@
 import Drum from './Drum.js'
 import Chanel from './Chanel.js'
+
+document.body.addEventListener('keypress', onKeyPress)
+document.querySelector('#recordBtn').addEventListener('click', onRecordBtnClick)
+document.querySelector('#playBtn').addEventListener('click', onPlayBtnClick)
+
+const chanelSelectors = document.querySelectorAll('.chanelSelector')
+
+const chanel1 = new Chanel()
+const chanel2 = new Chanel()
+const chanel3 = new Chanel()
+const chanel4 = new Chanel()
+
+let currentChanel
+
+chanelSelectors.forEach(button => {
+  button.addEventListener('click', selectChanel)
+})
+
+function selectChanel (ev) {
+  switch (ev.target.id) {
+    case 'recordChanel1':
+      currentChanel = chanel1
+      break
+    case 'recordChanel2':
+      currentChanel = chanel2
+      break
+    case 'recordChanel3':
+      currentChanel = chanel3
+      break
+    case 'recordChanel4':
+      currentChanel = chanel4
+      break
+  }
+}
+
+function onKeyPress (ev) {
+  let soundName
+  switch (ev.code) {
+    case 'KeyA':
+      soundName = 'clap'
+      break
+    case 'KeyS':
+      soundName = 'cowbell'
+      break
+    case 'KeyD':
+      soundName = 'hihat'
+      break
+    case 'KeyF':
+      soundName = 'kick'
+      break
+    case 'KeyG':
+      soundName = 'openhat'
+      break
+    case 'KeyH':
+      soundName = 'perc'
+      break
+    case 'KeyJ':
+      soundName = 'snare808'
+      break
+    case 'KeyK':
+      soundName = 'snareAnalog'
+      break
+    case 'KeyL':
+      soundName = 'tom'
+      break
+  }
+  if (soundName) {
+    currentChanel.addSound(new Drum(soundName))
+    currentChanel.playNewest()
+  }
+}
+
+function onRecordBtnClick () {
+  currentChanel.startRecording()
+}
+function onPlayBtnClick () {
+  currentChanel.playSounds()
+}
