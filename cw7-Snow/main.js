83417f269e7e0b474a2a2e4c4eb54074b652ed74
KboRodo <61941560+KboRodo@users.noreply.github.com>
2020-12-14 21:04:00 +0100

    dodano śnieg

--- cw7-Snow/main.js
+++ cw7-Snow/main.js
@@ -3,24 +3,26 @@ const c = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-function Snowflake (x, speedX, speedY) {
+function Snowflake (x, speedX, speedY, radius) {
   this.x = x
   this.y = 0
+  this.radius = radius
 
   this.speedX = speedX
   this.speedY = speedY
 
   this.draw = () => {
     c.beginPath()
-    c.arc(this.x, this.y, 30, 0, Math.PI * 2, false)
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
     c.strokeStyle = 'blue'
+    c.fillStyle = 'white'
     c.stroke()
   }
 
   this.update = () => {
     this.x += this.speedX
     this.y += this.speedY
-    if (this.x > innerWidth) {
+    if (this.x > innerWidth || this.x < 0) {
       this.speedX = -this.speedX
     }
     if (this.y > innerHeight || this.y < 0) {
@@ -33,11 +35,12 @@ function Snowflake (x, speedX, speedY) {
 
 const circleArray = []
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 300; i++) {
   const speedX = Math.random() * (3 - 1)
   const speedY = Math.random() * (6 - 2)
+  const radius = Math.random() * (30 - 10)
   const x = Math.random() * innerWidth
-  circleArray.push(new Snowflake(x, speedX, speedY))
+  circleArray.push(new Snowflake(x, speedX, speedY, radius))
 }
 
 function animate () {
